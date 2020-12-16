20b6ec11f1b475f9dd904d300f4d672c2c197f35
Taylor Ford <tford28@uncc.edu>
2020-12-16 17:29:45 -0500

    First commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+  user: any;
+
+  ngOnInit(): void {
+  }
+
+  logout(){
+    localStorage.removeItem('JWT_Token');
+  }
+}
